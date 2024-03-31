19850f7f277934a78707beaedbe47dffc2d94de4
Yashwant937363 <yashwantpoyrekar@gmail.com>
2024-03-31 10:42:51 +0530

    CORS fixed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { Server } = require("socket.io");
 const { UsersStore } = require("./store/sessionStore");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://gossip-app-dz7b.onrender.com",
   },
 });
 
@@ -30,7 +30,7 @@ io.on("connection", onConnection);
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gossip-app-dz7b.onrender.com",
   })
 );
 
